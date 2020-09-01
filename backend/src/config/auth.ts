9515196c2e583b116c1cc192daf7e4e25a7521e5
Vanderson Teodoro camatini <vandersoncamatini66@gmail.com>
2020-09-01 07:52:09 -0300

    Creating middlewares and modifying types of TS.

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'f215321837fb681f7e692e62ca0cf74a',
+        expiresIn: '1d',
+    },
+};
--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +12,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserServer {
@@ -27,8 +31,16 @@ class AuthenticateUserServer {
             throw new Error('Incorrect email/password combination.');
         }
 
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({}, secret, {
+            subject: user.id,
+            expiresIn,
+        });
+
         return {
             user,
+            token,
         };
     }
 }
