837f01dfe3b33c395068e582a0982ab14dd5d2c0
Vanderson Teodoro Camatini <vandersoncamatini66@gmail.com>
2020-10-05 21:40:56 -0300

    Add dashboard styles.

--- github-explorer/src/pages/Dashboard/index.tsx
+++ github-explorer/src/pages/Dashboard/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-import {Title} from './styles';
+import { Title, Form, Repositories } from './styles';
+
+import githubLogo from '../../assets/github-logo.svg';
 
 const Dashboard: React.FC = () =>{
-  return <Title>Dashboard</Title>;
+  return(
+    <>
+      <img src={githubLogo} alt="Github Explorer"/>
+      <Title>Github Explorer</Title>;
+      <Form>
+        <input placeholder="Digite o nome do repositório..."/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="repository">
+          <img src="https://avatars2.githubusercontent.com/u/37720776?s=460&u=c2366f04a4551e57365f0ef007e85c2f1edd4fe2&v=4" alt="avatar"/>
+          <div>
+            <strong>GoStack 13.0</strong>
+            <p>Um curso fodasticamente foda.</p>
+          </div>
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
--- github-explorer/src/styles/global.ts
+++ github-explorer/src/styles/global.ts
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
-import githubBackground from '../assets/github.svg';
+import githubBackground from '../assets/github-background.svg';
 
 export default createGlobalStyle`
+
   *{
     margin: 0;
     padding: 0;
@@ -17,6 +18,12 @@ export default createGlobalStyle`
 
   body, input, button {
     font: 16px Roboto, sans-serif;
+  }
+
+  #root{
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
   }
 
   button {
